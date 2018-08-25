44a78568a9786bc9f8b0257c96397b07efd038c6
Omar Mahdi <yin.omar.oa@gmail.com>
2018-08-25 14:30:09 +0400

    Added fadein/fadeout effects instead of slides

--- js/app.js
+++ js/app.js
@@ -1,32 +1,53 @@
-$(".main").onepage_scroll({
-    dotstyle: "scaleup",              // dotstyle let you choose which cool style you want to use, default is "fillup",
-    sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-    easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
-    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-    animationTime: 1500,             // AnimationTime let you define how long each section takes to animate
-    pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-    updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-    beforeMove: function (index) {
-        $(`section:eq(${index - 2})`).fadeToggle(1000);
-        $(`section:eq(${index - 1})`).fadeToggle(200);
-    },  // This option accepts a callback function. The function will be called before the page moves.
-    afterMove: function (index) { },   // This option accepts a callback function. The function will be called after the page moves.
-    loop: true,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-    keyboard: true,                  // You can activate the keyboard controls
-    responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
-    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
-    // the browser's width is less than 600, the fallback will kick in.
-    direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".
-});
+let currentPage = 0;
+const minPage = 0;
+const maxPage = 2;
 
-$('.down-arrow').on('click', () => {
-    $(".main").moveDown();
-});
+init();
 
-$('.right-arrow').on('click', () => {
-    $(".main").moveDown();
-});
+function init() {
+    $('.down-arrow').on('click', nextPage);
+    $('.right-arrow').on('click', nextPage);
+    $('.next-page').on('click', nextPage);
+    $('section:eq(0)').css('opacity', 1);
+    hidePages();
+    paginationNavigate();
+}
 
-$('.next-page').on('click', () => {
-    $(".main").moveDown();
-});
+// Changes to the next page
+function nextPage() {
+    if (currentPage < maxPage) {
+        $(`section:eq(${currentPage})`).css('opacity', 0);
+        $(`section:eq(${currentPage + 1})`).css('opacity', 1);
+        currentPage++;
+        paginationUpdate();
+    } else {
+        $(`section:eq(${currentPage})`).css('opacity', 0);
+        currentPage = 0;
+        $(`section:eq(${currentPage})`).css('opacity', 1);
+        paginationUpdate();
+    }
+}
+
+// Updates the dots on the right to be in syncw ith the current page
+function paginationUpdate() {
+    $('li').removeClass('current');
+    $(`li[index="${currentPage}"]`).addClass('current');
+}
+
+// Adds navigation functionality for the dots on the right
+function paginationNavigate() {
+    $('.dotstyle li').on('click', function () {
+        let index = $(this).attr('index');
+        $('section').css('opacity', 0);
+        $(`section:eq(${index})`).css('opacity', 1);
+        currentPage = index;
+        paginationUpdate();
+    });
+}
+
+// Hides all pages beyond the first page
+function hidePages() {
+    for (let i = 1; i < maxPage; i++) {
+        $(`section:eq(${i})`).css('opacity', 0);
+    }
+}
